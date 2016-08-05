d861651b54c4e2fc7b38aa3b57aa4d0c6f0f82d7
Batuhan Icoz <batuhanicoz@gmail.com>
2016-08-05 12:33:31 +0300

    added immutable support for redux-persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,7 @@ import { persistStore, autoRehydrate } from 'redux-persist';
 import devTools from 'remote-redux-devtools';
 import middleware from './../middleware/middlewareConfig';
 import reducer from './configureReducers';
+import immutableTransform from 'redux-persist-transform-immutable'
 
 export const configureStore = (onCompletion = () => {}) => {
 
@@ -23,7 +24,10 @@ export const configureStore = (onCompletion = () => {}) => {
     enhancer
   );
 
-  persistStore(store, {storage: AsyncStorage}, onCompletion);
+  persistStore(store, {
+    storage: AsyncStorage,
+    transforms: [immutableTransform()]
+  }, onCompletion);
 
   return store;
 
