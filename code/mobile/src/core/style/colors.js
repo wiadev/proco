b0638a9700d5992d4eac8cddac6deb9df422dc66
Gökhan Sarı <me@th0th.me>
2016-08-18 13:04:22 +0300

    Added brand colors.

--- code/mobile/src/core/style/colors.js
+++ code/mobile/src/core/style/colors.js
@@ -1,4 +1,10 @@
 const colors = {
+  primaryAlt: '#FFFFFF', // White
+  primary1: '#7837Af', // Proco Purple
+  primary2: '#FFC80B', // Proco Yellow
+  primary3: '#FF9E0B', // Proco Orange
+  secondary: '#91288C', // Proco Magenta
+
   danger: '#EA5644',
   warning: '#F5A611',
   success: '#19C4A6',
