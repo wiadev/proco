a9a57ffc50284473af039dece5996cc28dd11f69
Batuhan Icoz <batuhanicoz@gmail.com>
2016-09-03 12:52:32 +0300

    added getLoopsWithCache and getUserLoops methods, removed logging for Firebase

--- code/mobile/src/core/Api/index.js
+++ code/mobile/src/core/Api/index.js
@@ -1,5 +1,5 @@
 import Rebase from "re-base";
-import {AsyncStorage} from "react-native";
+import {AsyncStorage, Image} from "react-native";
 
 export const base = Rebase.createClass({
   apiKey: "AIzaSyCFOGhparb6dYAwoKtgvnHZ37hh0EARsOQ",
@@ -9,23 +9,44 @@ export const base = Rebase.createClass({
 });
 
 export const database = base.database();
-base.database.enableLogging(true);
+export const storage = base.storage();
 
 export const getFirebaseDataWithCache = ref => {
-  return new Promise((resolve) => {
-    const key = '@Proco:FDC:' + ref;
-    AsyncStorage.getItem(key).then(data => {
-      if (data !== null) return resolve(JSON.parse(data));
-      database.ref(ref)
-        .once('value')
-        .then(snap => snap.val())
-        .then(data => {
-          if (!data) return resolve(null);
-          AsyncStorage.setItem(key, JSON.stringify(data));
-          resolve(data);
-        });
+  const key = '@Proco:FDC:' + ref;
+  return AsyncStorage.getItem(key).then(data => {
+    if (data !== null) return JSON.parse(data);
+    return database.ref(ref)
+      .once('value')
+      .then(snap => snap.val())
+      .then(data => {
+        if (!data) return null;
+        AsyncStorage.setItem(key, JSON.stringify(data));
+        return data;
+      });
+  });
+};
+
+export const getLoopsWithCache = (uid, loop_key, count = 18) => {
+  const key = `@Proco:FSC:PLP:${uid}:${loop_key}`;
+  return AsyncStorage.getItem(key).then(data => {
+    if (data !== null) return JSON.parse(data);
+    const loopBaseRef = storage.ref(`users/loops/${uid}/${loop_key}`);
+    let files = [];
+    for (let i = 0; i < count; i++) files.push(loopBaseRef.child(`${i}.jpg`).getDownloadURL());
+    return Promise.all(files).then(links => {
+      links.forEach(link => Image.prefetch(link));
+      AsyncStorage.setItem(key, JSON.stringify(links));
+      return links;
     });
   });
+};
+
+export const getUserLoops = (uid, loop_key = null, count = 18) => {
+  if (!loop_key) return getLoopsWithCache(uid, loop_key, count);
+  return database.ref(`users/summary/${uid}/loop_key`) // Don't use cache here, it may change
+    .once('value')
+    .then(snap => snap.val())
+    .then(key => getLoopsWithCache(uid, key, count));
 };
 
 export const getUserSummary = uid => getFirebaseDataWithCache(`users/summary/${uid}`);
