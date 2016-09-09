c4e5707e6b522b1192e29517d4f3435514ad4e37
Batuhan Icoz <batuhanicoz@gmail.com>
2016-09-09 17:42:54 +0300

    deleted unneeded reducer code

--- code/mobile/src/modules/User/dataReducers/is.js
+++ code/mobile/src/modules/User/dataReducers/is.js
@@ -1,10 +1,7 @@
 import { assign } from '../../../core/utils';
 
 const reducer = (data) => {
-  const { blocked } = data;
-
-  return Promise.resolve(assign(data, {
-  }));
+  return Promise.resolve(data);
 };
 
 export default reducer;
