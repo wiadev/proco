a66932dc3853fdae743e6c2274e90b8e1150a934
Gökhan Sarı <me@th0th.me>
2016-09-10 16:30:19 +0300

    Make Filters great again.

--- code/mobile/src/components/Field/index.js
+++ code/mobile/src/components/Field/index.js
@@ -5,11 +5,16 @@ import {
   Switch,
   TouchableHighlight
 } from 'react-native';
+import _ from 'lodash';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  MKRangeSlider
+} from 'react-native-material-kit';
 
 import styles from './styles';
+import colors from '../../core/style/colors';
 
-const types = ['bool', 'text', 'link'];
+const types = ['bool', 'text', 'link', 'range', 'choice'];
 
 export default class Field extends React.Component {
   constructor(props) {
@@ -31,7 +36,7 @@ export default class Field extends React.Component {
       return (
         <TouchableHighlight onPress={() => this.props.onPress()} activeOpacity={0.96}>
           <View style={[this._getStyle(), this.props.style]}>
-            <Text style={styles.text}>{this.props.legend}</Text>
+            {this._renderLegend()}
 
             <Icon name="keyboard-arrow-right" style={styles.linkIcon} />
           </View>
@@ -39,12 +44,44 @@ export default class Field extends React.Component {
       );
     }
 
+    if (this.props.type === 'choice') {
+      return (
+        <TouchableHighlight onPress={() => this.props.onPress()} activeOpacity={0.96}>
+          <View style={[this._getStyle(), this.props.style]}>
+            {this._renderLegend()}
+
+            {this._renderChoiceCheck()}
+          </View>
+        </TouchableHighlight>
+      );
+    }
+
+    if (this.props.type === 'range') {
+      return (
+        <View style={[this._getStyle(), this.props.style]}>
+          <View style={styles.multiRowFieldTopRow}>
+            {this._renderLegend()}
+
+            <Text style={styles.highlightedText}>{this.state.value[0]} - {this.state.value[1]}</Text>
+          </View>
+
+          {this._renderValue()}
+        </View>
+      );
+    }
+
     return (
       <View style={[this._getStyle(), this.props.style]}>
-        <Text style={styles.text}>{this.props.legend}</Text>
+        {this._renderLegend()}
 
         {this._renderValue()}
       </View>
+    );
+  }
+
+  _renderLegend() {
+    return (
+      <Text style={styles.text}>{this.props.legend}</Text>
     );
   }
 
@@ -60,10 +97,37 @@ export default class Field extends React.Component {
         <Switch value={this.state.value} onValueChange={value => this._onValueChange(value)} disabled={this.props.readOnly} />
       );
     }
+
+    if (this.props.type === 'range') {
+      return (
+        <MKRangeSlider
+          min={this.props.minValue}
+          max={this.props.maxValue}
+          minValue={this.state.value[0]}
+          maxValue={this.state.value[1]}
+          onChange={value => this._onValueChange([_.round(value.min), _.round(value.max)])}
+          lowerTrackColor={colors.primary1}
+        />
+      );
+    }
+  }
+
+  _renderChoiceCheck() {
+    if (this.props.value) {
+      return (
+        <Icon name="check" style={styles.choiceIcon} />
+      );
+    }
   }
 
   _getStyle() {
     let fieldStyle = [styles.field];
+
+    if (this.props.type === 'range') {
+      fieldStyle.push(styles.multiRowField);
+    } else {
+      fieldStyle.push(styles.singleRowField);
+    }
 
     if (this.props.stickToPrevious) {
       fieldStyle.push(styles.stickToPrevious);
@@ -86,6 +150,8 @@ Field.propTypes = {
   readOnly: React.PropTypes.bool,
   legend: React.PropTypes.string,
   value: React.PropTypes.any,
+  minValue: React.PropTypes.number,
+  maxValue: React.PropTypes.number,
   onChange: React.PropTypes.func,
   onPress: React.PropTypes.func,
   stickToPrevious: React.PropTypes.bool,
--- code/mobile/src/scenes/Filters/styles.js
+++ code/mobile/src/scenes/Filters/styles.js
@@ -1,85 +1,20 @@
-import {
-  StyleSheet,
-  Dimensions,
-  PixelRatio,
-} from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import { getCorrectFontSizeForScreen } from '../../core/functions';
-
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+import colors from '../../core/style/colors';
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-    height,
-    width,
-    backgroundColor: '#f6f6f6',
-  },
-  btnSave: {
-    color: 'rgb(71,71,71)',
-    fontFamily: 'OpenSans',
-    fontSize: 13,
-    textAlign: 'center',
-  },
-  topMenuMid: {
-    flex: 0,
-    width: width * 50 / 100,
-    position: 'relative',
-    alignItems: 'center',
-  },
-  menuTitle: {
-    color: 'rgb(249,54,95)',
-    fontFamily: 'OpenSans',
-    fontSize: 17,
-    textAlign: 'center',
-  },
-  inputBox: {
-    marginTop: 14,
-    padding: 14,
-    width,
-    backgroundColor: 'white',
-    flex: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#dadbda',
-  },
-  inputBoxLeft: {
+  discoveryFilters: {
     flex: 1,
-    alignItems: 'flex-start',
-    width: width * 50 / 100,
+    backgroundColor: '#F6F6F6'
   },
-  inputBoxRight: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    width: width * 50 / 100,
-    flexDirection: 'row',
+  group: {
+    marginTop: 16
   },
-  pinkText: {
-    fontFamily: 'OpenSans-Light',
-    fontSize: 16,
-    color: 'rgb(249,59,95)',
-  },
-  blackText: {
-    fontFamily: 'OpenSans-Light',
-    fontSize: 16,
-    color: 'rgb(66,66,66)',
-  },
-  pinkHead: {
-    backgroundColor: 'rgb(249,59,95)',
-    padding: 14,
-    width,
-    flex: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  pinkHeadText: {
-    fontFamily: 'OpenSans-Light',
-    fontSize: 13,
-    color: 'white',
-  },
+  sectionTitle: {
+    marginVertical: 10,
+    paddingHorizontal: 16,
+    fontSize: 12
+  }
 });
 
 export default styles;
