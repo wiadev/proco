c8fcbbea3dc8165fd741c10d2e53a23beefa674f
Gökhan Sarı <me@th0th.me>
2016-09-13 16:51:29 +0300

    Enable passing all props to Text in custom Text component.

--- code/mobile/src/components/Text/index.js
+++ code/mobile/src/components/Text/index.js
@@ -6,7 +6,7 @@ import styles from './styles';
 export default class CustomText extends React.Component {
   render() {
     return (
-      <Text style={[styles.customText, this.props.style]}>{this.props.children}</Text>
+      <Text {...this.props} style={[styles.customText, this.props.style]}>{this.props.children}</Text>
     );
   }
 }
