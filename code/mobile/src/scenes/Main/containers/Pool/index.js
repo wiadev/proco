7067f47792aa2c2384ddcc976bc3688d89e44b38
Batuhan Icoz <batuhanicoz@gmail.com>
2016-09-13 11:05:54 +0300

    Fixed #100

--- code/mobile/src/scenes/Main/containers/Pool/index.js
+++ code/mobile/src/scenes/Main/containers/Pool/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 import {connect} from "react-redux";
-import {
-  View
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import {View} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import Swiper from "react-native-swiper";
-
+import {action} from "../../../../modules/Pool/actions";
 import PoolItem from "../../../../components/PoolItem";
 import Card from "../../../../components/Card";
 import PermissionModal from "../../../../components/PermissionModal";
-import {action} from '../../../../modules/Pool/actions';
-import styles from './styles';
+import styles from "./styles";
 
 @connect(state => ({auth: state.auth, permissions: state.permissions, pool: state.pool}))
 export default class Pool extends React.Component {
@@ -18,14 +15,15 @@ export default class Pool extends React.Component {
     super(props);
 
     this.state = {
-      current: null,
+      current: 0,
+      previousPoolData: {},
     };
   }
 
   render() {
     if (this.props.permissions.location !== 'authorized') {
       return (
-        <PermissionModal type="location" />
+        <PermissionModal type="location"/>
       );
     }
 
@@ -39,15 +37,21 @@ export default class Pool extends React.Component {
           loadMinimalSize={2}
           ref="poolswiper"
           onMomentumScrollEnd={(e, state) => {
+
             const items = Object.keys(this.props.pool.items);
-            this.props.dispatch(action(items[state.index - 1], 'skip'));
-            this.setState({current: items[state.index]});
+            const {uid = items[state.index - 1], act = 'skip', payload = null} = this.state.previousPoolData;
+            this.props.dispatch(action(uid, act, payload));
+            this.setState({
+              current: state.index,
+              previousPoolData: {},
+            });
+
           }}
         >
           {this._renderPoolItems()}
         </Swiper>
 
-        <Icon name="keyboard-arrow-up" style={styles.upperMenuIcon} />
+        <Icon name="keyboard-arrow-up" style={styles.upperMenuIcon}/>
       </View>
     );
   }
@@ -57,18 +61,28 @@ export default class Pool extends React.Component {
 
     if (poolItems.length < 1) {
       return (
-        <Card label="No one seems to be nearby" noClose={true} />
+        <Card label="No one seems to be nearby" noClose={true}/>
       );
     }
 
     return poolItems.map((poolItemKey, i) => {
       return (
-        <PoolItem key={poolItemKey} isMounted={i === 0} data={this.props.pool.items[poolItemKey]} onComplete={(uid, act, payload) => this._doneWithPoolItem(uid, act, payload)} />
+        <PoolItem key={poolItemKey} isMounted={i === 0} data={this.props.pool.items[poolItemKey]}
+                  onComplete={(uid, act, payload) => this._doneWithPoolItem(uid, act, payload)}
+        />
       );
     });
   }
 
   _doneWithPoolItem(uid, act, payload) {
-    this.props.dispatch(action(uid, act, payload));
+    this.setState({
+      previousPoolData: {
+        uid,
+        act,
+        payload,
+      },
+    });
+
+    this.refs.poolswiper.scrollBy(1);
   }
 }
