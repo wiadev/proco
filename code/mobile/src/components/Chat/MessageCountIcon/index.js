20312fdcfd48966b30f946494697eaaa43b47a1b
Gökhan Sarı <me@th0th.me>
2016-09-21 06:02:28 +0300

    Fix unread threads icon.

--- code/mobile/src/components/Chat/MessageCountIcon/index.js
+++ code/mobile/src/components/Chat/MessageCountIcon/index.js
@@ -10,9 +10,10 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
-@connect(state => ({userInfo: state.api.data.userInfo}))
+@connect(state => ({unseenThreadsCount: state.api.data.userThreads.unseen.length}))
 export default class MessageCountIcon extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <TouchableOpacity onPress={Actions.ConversationList} activeOpacity={0.6} style={styles.messageCountIcon}>
         <Icon name="comment" style={styles.icon} />
@@ -29,15 +30,15 @@ export default class MessageCountIcon extends React.Component {
   }
 
   _renderUnreadMessageCount() {
-    if (this.props.userInfo.unread_messages > 99) {
+    if (this.props.unseenThreadsCount > 99) {
       return '99+';
     } else {
-      return this.props.userInfo.unread_messages;
+      return this.props.unseenThreadsCount;
     }
   }
 
   _renderUnreadIndicator() {
-    if (this.props.userInfo.unread_messages !== 0) {
+    if (this.props.unseenThreadsCount !== 0) {
       return (
         <View style={styles.unreadIndicator}>
           <View style={styles.unreadIndicatorInner} />
