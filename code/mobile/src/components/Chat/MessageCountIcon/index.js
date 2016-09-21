61b74dcc3364ffbb527e2195856ac5cfd4a5fee1
Gökhan Sarı <me@th0th.me>
2016-09-22 02:24:37 +0300

    [WIP] New MessageCountIcon with avatars.

--- code/mobile/src/components/Chat/MessageCountIcon/index.js
+++ code/mobile/src/components/Chat/MessageCountIcon/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {
   View,
+  Image,
   Text,
   TouchableOpacity
 } from 'react-native';
@@ -10,16 +11,36 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
-@connect(state => ({unseenThreadsCount: state.api.data.userThreads.unseen.length}))
+@connect(state => ({threads: state.api.data.userThreads, profiles: state.profiles.profiles}))
 export default class MessageCountIcon extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      unseenThreads: {},
+      users: []
+    };
+  }
+
+  componentWillMount() {
+    this.props.threads.unseen.map(threadId => this._checkUnseenThread(threadId));
+  }
+
+  componentDidUpdate() {
+    this.props.threads.unseen.map(threadId => this._checkUnseenThread(threadId));
+  }
+
   render() {
-    console.log(this.props);
     return (
       <TouchableOpacity onPress={Actions.ConversationList} activeOpacity={0.6} style={styles.messageCountIcon}>
+        <View style={styles.avatars}>
+          {this.state.users.map((userId, key) => this._renderAvatar(userId, key))}
+        </View>
+
         <Icon name="comment" style={styles.icon} />
 
-        <View style={styles.contentWrapper}>
-          <View style={styles.content}>
+        <View style={styles.iconContentWrapper}>
+          <View style={styles.iconContent}>
             <Text style={styles.messagesCount}>{this._renderUnreadMessageCount()}</Text>
           </View>
         </View>
@@ -30,20 +51,60 @@ export default class MessageCountIcon extends React.Component {
   }
 
   _renderUnreadMessageCount() {
-    if (this.props.unseenThreadsCount > 99) {
+    if (this.props.threads.unseen.length > 99) {
       return '99+';
     } else {
-      return this.props.unseenThreadsCount;
+      return this.props.threads.unseen.length;
     }
   }
 
   _renderUnreadIndicator() {
-    if (this.props.unseenThreadsCount !== 0) {
+    if (this.props.threads.unseen.length !== 0) {
       return (
         <View style={styles.unreadIndicator}>
           <View style={styles.unreadIndicatorInner} />
         </View>
       );
+    }
+  }
+
+  _renderAvatar(userId, key) {
+    if (this.props.profiles.hasOwnProperty(userId)) {
+      return (
+        <Image
+          key={key}
+          source={{uri: this.props.profiles[userId].avatar}}
+          style={styles.avatar}
+        />
+      );
+    }
+  }
+
+  _addUnseenThread(threadId) {
+    const timestamp = this.props.threads.threads[threadId].last_message.createdAt;
+    const userId = this.props.threads.threads[threadId].people[0];
+
+    let newUsers = this.state.users.slice();
+
+    newUsers.push(userId);
+
+    this.setState({
+      users: newUsers.slice(-3),
+      unseenThreads: Object.assign({}, this.state.unseenThreads, {
+        [threadId]: timestamp
+      })
+    });
+  }
+
+  _checkUnseenThread(threadId) {
+    if (Object.keys(this.state.unseenThreads).indexOf(threadId) === -1) {
+      this._addUnseenThread(threadId);
+      return;
+    }
+
+    if (this.props.threads.threads[threadId].last_message.createdAt !== this.state.unseenThreads[threadId]) {
+      this._addUnseenThread(threadId);
+      return;
     }
   }
 }
