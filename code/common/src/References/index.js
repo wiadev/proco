36e59f8d056b7b1030e07c4c47630f628137b1de
Batuhan Icoz <batuhanicoz@gmail.com>
2016-10-03 13:07:16 +0300

    removed references from common library

--- code/common/src/References/index.js
+++
@@ -1,7 +0,0 @@
-import ServerActionResults from './ServerActionResults';
-import User from './User';
-
-module.exports = {
-  ServerActionResults,
-  User
-};
--- code/common/src/index.js
+++ code/common/src/index.js
@@ -1,9 +1,7 @@
-import References from './References';
 import Utils from './Utils';
 import Validations from './Validations';
 
 module.exports = {
-  References,
   Utils,
   Validations,
 };
