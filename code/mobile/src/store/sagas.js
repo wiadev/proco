cd8f132c703a465f7d4a743f3cda782e33c8078b
Gökhan Sarı <meisth0th@gmail.com>
2016-11-07 04:59:32 +0300

    Fix lil import case mistakes in some files.

--- code/mobile/src/store/sagas.js
+++ code/mobile/src/store/sagas.js
@@ -1,6 +1,6 @@
 import authSagas from '../core/auth/sagas';
 import locationSagas from '../core/location';
-import poolSagas from '../modules/pool/sagas';
+import poolSagas from '../modules/Pool/sagas';
 import userSagas from '../modules/user/sagas';
 
 export default function* sagas() {
