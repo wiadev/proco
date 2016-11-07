b2a1617c212d32ac1b860e16a4140cfc2e4b87d8
Gökhan Sarı <meisth0th@gmail.com>
2016-11-07 06:25:31 +0300

    Add “You need to enable location” modal to Main.

--- code/mobile/src/scenes/Main/styles.js
+++ code/mobile/src/scenes/Main/styles.js
@@ -1,12 +1,26 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  mainScreen: {
-    position: 'relative',
-    flex: 1
-  },
   swiperSlide: {
     flex: 1
+  },
+  // modal
+  modalContent: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  topImage: {
+    alignSelf: 'center',
+    width: 120,
+    height: 120,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  buttonWrapper: {
+    marginTop: 20
   }
 });
 
